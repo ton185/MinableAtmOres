7c273fece939d8331cd16ffe10054162e4e4eec9
ton185 <174216122+ton185@users.noreply.github.com>
2025-01-22 19:17:24 +0000

    Update MixinBlockBreak.java

--- src/main/java/com/name/matmores/mixin/MixinBlockBreak.java
+++ src/main/java/com/name/matmores/mixin/MixinBlockBreak.java
@@ -15,7 +15,6 @@ public class MixinBlockBreak {
     @Inject(at = @At("RETURN"), method = "on(Lnet/neoforged/neoforge/event/level/BlockEvent$BreakEvent;)V", remap = false)
     private static void on(BlockEvent.BreakEvent event, CallbackInfo ci) {
         BlockState state = event.getState();
-        Matmores.LOGGER.warn(String.valueOf(state.getDestroySpeed(event.getLevel(), event.getPos())));
         if (state.is(TagRegistry.ALLTHEMODIUM_ORE) || state.is(TagRegistry.VIBRANIUM_ORE) || state.is(TagRegistry.UNOBTAINIUM_ORE) || (state.is(TagRegistry.OTHER_PROTECTION) && event.getLevel().getBiome(event.getPos()).is(TagRegistry.OTHER_BIOMES))) {
             event.setCanceled(false);
         }

2e6066415df5b1ad59dde6b2eb121069486d036e
ton185 <174216122+ton185@users.noreply.github.com>
2025-01-22 18:37:58 +0000

    Add files via upload

--- src/main/java/com/name/matmores/mixin/MixinBlockBreak.java
+++ src/main/java/com/name/matmores/mixin/MixinBlockBreak.java
@@ -3,7 +3,7 @@ package com.name.matmores.mixin;
 import com.name.matmores.Matmores;
 import com.thevortex.allthemodium.events.BlockBreak;
 import com.thevortex.allthemodium.registry.TagRegistry;
-import net.minecraftforge.event.level.BlockEvent;
+import net.neoforged.neoforge.event.level.BlockEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BlockBreak.class)
 public class MixinBlockBreak {
-    @Inject(at = @At("RETURN"), method = "on(Lnet/minecraftforge/event/level/BlockEvent$BreakEvent;)V", remap = false)
+    @Inject(at = @At("RETURN"), method = "on(Lnet/neoforged/neoforge/event/level/BlockEvent$BreakEvent;)V", remap = false)
     private static void on(BlockEvent.BreakEvent event, CallbackInfo ci) {
         if (event.getState().is(TagRegistry.ALLTHEMODIUM_ORE) || event.getState().is(TagRegistry.VIBRANIUM_ORE) || event.getState().is(TagRegistry.UNOBTAINIUM_ORE)) {
             event.setCanceled(false);

4d18c9331750b8bbb47edc94a48bc1b2c8a3a752
ton185 <174216122+ton185@users.noreply.github.com>
2025-06-19 08:05:02 +0100

    Update MixinAllTheModiumOre.java

--- src/main/java/com/name/matmores/mixin/MixinAllTheModiumOre.java
+++ src/main/java/com/name/matmores/mixin/MixinAllTheModiumOre.java
@@ -1,29 +1,22 @@
 package com.name.matmores.mixin;
 
-import com.name.matmores.Matmores;
 import com.thevortex.allthemodium.blocks.Allthemodium_Ore;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.injection.*;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Allthemodium_Ore.class)
-public class MixinAllTheModiumOre {
-    @ModifyConstant(method = "<init>", constant = @Constant(floatValue = -1.0F))
-    private static float changeHardness(float hardness) {
-        return 100f;
+public class MixinAllTheModiumOre extends Block {
+    public MixinAllTheModiumOre(Properties p_i48440_1_) {
+        super(p_i48440_1_);
     }
 
-    /**
-     * @author name
-     * @reason Allow fake players to mine the ore
-     */
     @Overwrite(remap = false)
-    public boolean canEntityDestroy(BlockState state, BlockGetter world, BlockPos pos, Entity player) {
-        return true;
+    public boolean canHarvestBlock(BlockState state, IBlockReader world, BlockPos pos, PlayerEntity player) {
+        return super.canHarvestBlock(state, world, pos, player); // not 100% sure if it will work like this, might need return true
     }
 }

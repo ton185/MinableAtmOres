bfcd9944b8ca90c7bd9dd332477ee7034755d788
ton185 <174216122+ton185@users.noreply.github.com>
2025-06-19 08:05:34 +0100

    Update MixinBlockBreak.java

--- src/main/java/com/name/matmores/mixin/MixinBlockBreak.java
+++ src/main/java/com/name/matmores/mixin/MixinBlockBreak.java
@@ -1,20 +1,18 @@
 package com.name.matmores.mixin;
 
-import com.name.matmores.Matmores;
 import com.thevortex.allthemodium.events.BlockBreak;
-import com.thevortex.allthemodium.registry.TagRegistry;
-import net.minecraftforge.event.level.BlockEvent;
+import com.thevortex.allthemodium.reference.Reference;
+import net.minecraftforge.event.world.BlockEvent;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BlockBreak.class)
 public class MixinBlockBreak {
-    @Inject(at = @At("RETURN"), method = "on(Lnet/minecraftforge/event/level/BlockEvent$BreakEvent;)V", remap = false)
+    @Inject(at = @At("RETURN"), method = "on", remap = false)
     private static void on(BlockEvent.BreakEvent event, CallbackInfo ci) {
-        if (event.getState().is(TagRegistry.ALLTHEMODIUM_ORE) || event.getState().is(TagRegistry.VIBRANIUM_ORE) || event.getState().is(TagRegistry.UNOBTAINIUM_ORE)) {
+        if (event.getState().getBlock().getTags().contains(Reference.ORETYPE) || event.getState().getBlock().getTags().contains(Reference.ORETYPE2) || event.getState().getBlock().getTags().contains(Reference.ORETYPE3)) {
             event.setCanceled(false);
         }
     }
